980468efed54ca2e296c04a09a04bf82d127771d
ElijahMcKay <elijahmckay10@gmail.com>
2020-03-03 15:48:27 -0600

    Working more on displaying error messages to the user

--- src/components/Directions.jsx
+++ src/components/Directions.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
+import { createGlobalStyle } from 'styled-components';
 
 const Directions = props => {
 
@@ -11,42 +12,54 @@ const Directions = props => {
 		title: '',
 		description: '',
 		players: '',
-		error_msg: '',	
+		error_msg: '',
+		isError: false
 	})
 
 	const moveDirection = e => {
 		e.preventDefault();
+		console.log(e.keyCode)
+		if(e.keyCode === 78 || e.keyCode === 69 || e.keyCode === 83 || e.keyCode === 87) {
+			console.log('pass')
+			setRoomInfo({ ...location, isError: false })
+			setLocation({ direction: String.fromCharCode(e.keyCode).toLowerCase() });
 
-		setLocation({ direction: String.fromCharCode(e.keyCode).toLowerCase() });
-		console.log(location)
-		axiosWithAuth()
-		.post('api/adv/move/', location)
-		.then(res => {
-			setRoomInfo({
-				title: res.data.title,
-				description: res.data.description,
-				players: res.data.player,
-				error_msg: res.data.error_msg,
+			axiosWithAuth()
+			.post('api/adv/move/', location)
+			.then(res => {
+				setRoomInfo({
+					title: res.data.title,
+					description: res.data.description,
+					players: res.data.player,
+					error_msg: res.data.error_msg,
+				})
+				console.log('moveNorth Res: ', res.data);
 			})
-			console.log('moveNorth Res: ', res.data);
-		})
-		.catch(err => {
-			console.log('North Error: ', err.res);
-		});
+			.catch(err => {
+				console.log('North Error: ', err.res);
+			});
+		} else {
+			setRoomInfo({
+				...roomInfo,
+				isError: true
+			})
+		}
 	};
 
 	return (
 		<div className="Direction-Buttons">
 			<label htmlFor="direction">Direction</label>
-			<input 
-			className="direction" 
-			onKeyDown={moveDirection} 
-			value={location.direction}
-			onChange={moveDirection} />
+			<input
+				className="direction"
+				onKeyDown={moveDirection} 
+				value={location.direction}
+				onChange={moveDirection} 
+			/>
+			{roomInfo.isError ? <p>That isn't a valid direction.</p> : null }
 			{location.direction ? <p>Last Move: {location.direction}</p> : null }
+			{roomInfo.error_msg ? <p>{roomInfo.error_msg}</p> : null }
 			<p>{roomInfo.title}</p>
 			<p>{roomInfo.description}</p>
-			<p>{roomInfo.error_msg}</p>
 		</div>
 	);
 };
