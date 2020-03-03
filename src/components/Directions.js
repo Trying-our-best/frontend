1a89eea47c555ec30b904a6db530a06d57126f8f
Alec Blakeley <alecblkly@gmail.com>
2020-03-03 13:24:13 -0500

    Can move in SEW.

--- src/components/Directions.js
+++ src/components/Directions.js
@@ -17,10 +17,58 @@ const Directions = props => {
          });
    };
 
+   const moveSouth = e => {
+      e.preventDefault();
+      setLocation({ direction: 's' });
+      axiosWithAuth()
+         .post('api/adv/move/', location)
+         .then(res => {
+            console.log('moveNorth Res: ', res.data);
+         })
+         .catch(err => {
+            console.log('North Error: ', err.res);
+         });
+   };
+
+   const moveEast = e => {
+      e.preventDefault();
+      setLocation({ direction: 'e' });
+      axiosWithAuth()
+         .post('api/adv/move/', location)
+         .then(res => {
+            console.log('moveNorth Res: ', res.data);
+         })
+         .catch(err => {
+            console.log('North Error: ', err.res);
+         });
+   };
+
+   const moveWest = e => {
+      e.preventDefault();
+      setLocation({ direction: 'w' });
+      axiosWithAuth()
+         .post('api/adv/move/', location)
+         .then(res => {
+            console.log('moveNorth Res: ', res.data);
+         })
+         .catch(err => {
+            console.log('North Error: ', err.res);
+         });
+   };
+
    return (
       <div className="Direction-Buttons">
          <button className="North" onClick={moveNorth}>
             North
+         </button>
+         <button className="South" onClick={moveSouth}>
+            South
+         </button>
+         <button className="East" onClick={moveEast}>
+            East
+         </button>
+         <button className="West" onClick={moveWest}>
+            West
          </button>
       </div>
    );
