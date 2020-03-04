defa084d5a14eeb29448dcd63fde47f48786468e
Megan <megan.miller@smoldev.dev>
2020-03-04 14:28:15 -0600

    Styling the player List component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,40 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import React from "react"
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 
 // component imports
-import Login from '../src/components/Login';
-import Register from '../src/components/Register';
-import Game from '../src/components/Game';
-import NavBar from './components/NavBar/NavBar'
+import Login from "../src/components/Login"
+import Register from "../src/components/Register"
+import Game from "../src/components/Game"
+import NavBar from "./components/NavBar/NavBar"
 
 // styling & image imports
-import './App.css'
-import StoneTexture from './assets/stone-texture.png'
-
+import "./App.css"
+import StoneTexture from "./assets/stone-texture.png"
 
 function App() {
-
-   return localStorage.getItem('token') ? (
-      <Router>
-         <div className="App" style={{ "backgroundImage": `url(${StoneTexture})`, "height": "100vh"}}>
-            <NavBar />
-            <h2>CS MUD</h2>
-            <Link to="/game">Game</Link>
-            <Route path="/game" component={Game} />
-         </div>
-      </Router>
-   ) : (
-      <Router>
-         <div className="App">
-            <h2>CS MUD</h2>
-            <Link to="/login">Login </Link>
-            <Link to="/register">Register </Link>
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-         </div>
-      </Router>
-   );
+  return localStorage.getItem("token") ? (
+    <Router>
+      <div
+        className="App"
+        style={{ backgroundImage: `url(${StoneTexture})`, height: "100vh" }}
+      >
+        <NavBar />
+        <h2>CS MUD</h2>
+        <Link to="/game">Game</Link>
+        <Route path="/game" component={Game} />
+      </div>
+    </Router>
+  ) : (
+    <Router>
+      <div className="App">
+        <h2>CS MUD</h2>
+        <Link to="/login">Login </Link>
+        <Link to="/register">Register </Link>
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
