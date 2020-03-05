75ad5f3fac742bd9e4ead14389e97fb63b2b47ba
Elijah McKay <elijahmckay10@gmail.com>
2020-03-05 12:44:34 -0600

    fixed soft error where we were editing state without using this.setState()

--- src/components/Canvas/Canvas.jsx
+++ src/components/Canvas/Canvas.jsx
@@ -51,47 +51,85 @@ export default class Canvas extends Component {
               }
           })
         })
-        .catch(err => err.err) 
-
-        console.log(this.state.room.name)
+        .catch(err => err.err)  
 
         setInterval(() => {
             this.drawTorch(this.state.x, this.state.y, this.state.width, this.state.height);
-            console.log('rerender')
+            // console.log('rerender')
         }, 1000 / 60);
 
-        // document.addEventListener('keydown', (e) => {
-
-        //     // if(e.key === 'w') {
-        //     //     this.state.y -= 100
-
-        //     // } else if(e.key === 'a') {
-        //     //     this.state.x -= 100
-
-        //     // } else if (e.key === 's') {
-        //     //     this.state.y += 100
-
-        //     // } else if (e.key === 'd') {
-        //     //     this.state.x += 100
-
-        //     // }
-        // })
-
         document.addEventListener('keydown', (e) => {
+            let temp;
+            const ctx = this.refs.canvas.getContext('2d');
+            // checking for which key is pressed
             if (e.key === "w") {
-              this.state.y -= 100
-              this.state.location.direction = 'n'
+              // setting temp var to capture value of y
+              temp = this.state.y
+              temp -= 100
+
+              // if y value - 100 is still greater than zero, do movement as normal
+              if (temp > 0) {
+
+                // console.log(this.state.y)sss
+                this.setState({
+                  ...this.state,
+                  y: temp,
+                  location: {
+                    direction: 'n'
+                  }
+                })
+              }
+
             } else if (e.key === 'a') {
-              this.state.x -= 100
-              this.state.location.direction = 'w' 
+              // setting temp var to capture value of y
+              temp = this.state.x
+              temp -= 100
+              
+              // if x value - 100 is still greater than zero, do movement as normal
+              if (temp > 0) {
+
+                this.setState({
+                  ...this.state,
+                  x: temp,
+                  location: {
+                    direction: 'w'
+                  }
+                })
+              }
+
             } else if (e.key === 's') {
-              this.state.y += 100
-              this.state.location.direction = 's'
+              // setting temp var to capture value of y
+              temp = this.state.y + 100
+              // temp += 100
+              // if y value - 100 is still greater than zero, do movement as normal
+              if (temp < ctx.canvas.height) {
+                console.log('made it')
+                this.setState({
+                  ...this.state,
+                  y: this.state.y + 100,
+                  location: {
+                    direction: 's'
+                  }
+                })
+              }
+
+
+
             } else if (e.key === 'd') {
-              this.state.x += 100
-              this.state.location.direction = 'e' 
+              temp = this.state.x
+              temp += 100
+              // if x value - 100 is still greater than zero, do movement as normal
+              if (temp < ctx.canvas.width) {
+
+                this.setState({
+                  ...this.state,
+                  x: temp,
+                  location: {
+                    direction: 'e'
+                  }
+                })
+              }
             }
-        
             
             axiosWithAuth()
               .post("api/adv/move/", this.state.location)
@@ -105,17 +143,18 @@ export default class Canvas extends Component {
                         players: res.data.players
                     }
                 })
-                console.log("moveNorth Res: ", res.data)
+                // console.log("moveNorth Res: ", res.data)
               })
               .catch(err => {
-                console.log("North Error: ", err.res)
+                // console.log("North Error: ", err.res)
               })
           })
         }
 
-
-    render() {
-        console.log(this.state.room)
+        
+        render() {
+          console.log(this.state)
+        // console.log(this.state.room)
         return (
 
             <div>
