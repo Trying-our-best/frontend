0035718efda9d9b60ff8b970f176cff4262097a5
jambis <imjaamees@gmail.com>
2020-03-05 16:32:25 -0600

    works flawlessly

--- src/components/Canvas/Canvas.jsx
+++ src/components/Canvas/Canvas.jsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect, Component } from "react"
+import React, { useState, useEffect, Component } from "react";
 
-import Player from "../../assets/torch.png"
-import PlayerList from "../playerList/playerList"
+import Player from "../../assets/torch.png";
+import PlayerList from "../playerList/playerList";
 
-import { axiosWithAuth } from "../../utils/axiosWithAuth"
+import { axiosWithAuth } from "../../utils/axiosWithAuth";
 
-import Directions from "../Directions"
+import Directions from "../Directions";
 
 export default class Canvas extends Component {
   state = {
@@ -13,8 +13,8 @@ export default class Canvas extends Component {
     y: 15,
     torchW: 15,
     torchH: 15,
-    tileW: 10,
-    tileH: 10,
+    tileW: 15,
+    tileH: 15,
     location: {
       direction: null
     },
@@ -22,60 +22,104 @@ export default class Canvas extends Component {
       name: null,
       currentRoom: null,
       roomDescription: null,
-      players: null
+      players: null,
+      error_msg: null
     },
     gameMapArr: []
-  }
+  };
 
   drawCanvas = () => {
-    const ctx = this.refs.canvas.getContext("2d")
-    //draw tiles that make up game map
-    //beign by looping through each tile
+    const ctx = this.refs.canvas.getContext("2d");
     for (let y = 0; y <= 47; y++) {
-      //for each row, go left to right
       for (let x = 0; x <= 47; x++) {
-        //switch statement lets us choose which color to draw curent tile with
-        //find value at corresponding game map index by y*mapW and add x
-        // switch (this.state.gameMapArr[y * 47 + x]) {
-        //   case 0:
-        //     ctx.fillStyle = "#999999"
-        //     break
-        //   default:
-        //     ctx.fillStyle = "#eeeeee"
-        // }
-
-        // for length of array find odd rows and remove them.
-
         if (this.state.gameMapArr[y * 47 + x]) {
-          ctx.fillStyle = "#eee"
+          ctx.fillStyle = "#eee";
         } else {
-          ctx.fillStyle = "#999"
+          ctx.fillStyle = "#999";
         }
-        //draw rectangle at coresponding position tile
         ctx.fillRect(
           x * this.state.tileW,
           y * this.state.tileH,
           this.state.tileW,
           this.state.tileH
-        )
+        );
       }
     }
-  }
+  };
 
   drawTorch = (x, y, width, height) => {
-    const ctx = this.refs.canvas.getContext("2d")
+    const ctx = this.refs.canvas.getContext("2d");
     //ctx.clearRect(0, 0, this.refs.canvas.width, this.refs.canvas.height)
 
-    const img = new Image()
-    img.src = Player
+    const img = new Image();
+    img.src = Player;
 
-    ctx.drawImage(img, x, y, width, height)
-  }
+    ctx.drawImage(img, x, y, width, height);
+  };
+
+  verifyMovement = () => {
+    axiosWithAuth()
+      .post("api/adv/move/", this.state.location)
+      .then(res => {
+        console.log(res.data);
+        let temp = 0;
+        this.setState({
+          ...this.state,
+          room: {
+            name: res.data.name,
+            currentRoom: res.data.title,
+            roomDescription: res.data.description,
+            players: res.data.players,
+            error_msg: res.data.error_msg
+          }
+        });
+        if (res.data.error_msg === "") {
+          console.log(res.data.error_msg);
+          switch (this.state.location.direction) {
+            case "w":
+              temp = this.state.x;
+              temp -= 30;
+              this.setState({
+                ...this.state,
+                x: temp
+              });
+              break;
+            case "e":
+              temp = this.state.x;
+              temp += 30;
+              this.setState({
+                ...this.state,
+                x: temp
+              });
+              break;
+            case "n":
+              temp = this.state.y;
+              temp -= 30;
+              this.setState({
+                ...this.state,
+                y: temp
+              });
+              break;
+            case "s":
+              temp = this.state.y + 30;
+              this.setState({
+                ...this.state,
+                y: this.state.y + 30
+              });
+              break;
+            default:
+              break;
+          }
+        }
+      })
+      .catch(err => err.res);
+  };
 
   componentDidMount = () => {
     axiosWithAuth()
       .get("/api/adv/init/")
       .then(res => {
+        let temp = 0;
         this.setState({
           ...this.state,
           room: {
@@ -84,9 +128,9 @@ export default class Canvas extends Component {
             name: res.data.name,
             players: res.data.players
           }
-        })
+        });
       })
-      .catch(err => err.err)
+      .catch(err => err.err);
 
     axiosWithAuth()
       .get("/api/adv/gamemap/")
@@ -94,8 +138,8 @@ export default class Canvas extends Component {
         this.setState({
           ...this.state,
           gameMapArr: res.data.gameMap.flat()
-        })
-      })
+        });
+      });
 
     setInterval(() => {
       this.drawTorch(
@@ -103,96 +147,58 @@ export default class Canvas extends Component {
         this.state.y,
         this.state.torchW,
         this.state.torchH
-      )
-    }, 1000 / 30)
+      );
+    }, 1000 / 30);
 
     document.addEventListener("keydown", e => {
-      let temp
-      const ctx = this.refs.canvas.getContext("2d")
       // checking for which key is pressed
       if (e.key === "w") {
         // setting temp var to capture value of y
-        temp = this.state.y
-        temp -= 30
 
         // if y value - 100 is still greater than zero, do movement as normal
-        if (temp > 0) {
-          // console.log(this.state.y)sss
-          this.setState({
-            ...this.state,
-            y: temp,
-            location: {
-              direction: "n"
-            }
-          })
-        }
+
+        // console.log(this.state.y)sss
+        this.setState({
+          ...this.state,
+          location: {
+            direction: "n"
+          }
+        });
+        this.verifyMovement();
       } else if (e.key === "a") {
-        // setting temp var to capture value of y
-        temp = this.state.x
-        temp -= 30
-
-        // if x value - 100 is still greater than zero, do movement as normal
-        if (temp > 0) {
-          this.setState({
-            ...this.state,
-            x: temp,
-            location: {
-              direction: "w"
-            }
-          })
-        }
+        this.setState({
+          ...this.state,
+          location: {
+            direction: "w"
+          }
+        });
+        this.verifyMovement();
       } else if (e.key === "s") {
-        // setting temp var to capture value of y
-        temp = this.state.y + 30
-        // temp += 100
-        // if y value - 100 is still greater than zero, do movement as normal
-        if (temp < ctx.canvas.height) {
-          this.setState({
-            ...this.state,
-            y: this.state.y + 30,
-            location: {
-              direction: "s"
-            }
-          })
-        }
-      } else if (e.key === "d") {
-        temp = this.state.x
-        temp += 30
-        // if x value - 100 is still greater than zero, do movement as normal
-        if (temp < ctx.canvas.width) {
-          this.setState({
-            ...this.state,
-            x: temp,
-            location: {
-              direction: "e"
-            }
-          })
-        }
-      }
+        this.setState({
+          ...this.state,
+          location: {
+            direction: "s"
+          }
+        });
 
-      axiosWithAuth()
-        .post("api/adv/move/", this.state.location)
-        .then(res => {
-          console.log(res.data)
-          this.setState({
-            ...this.state,
-            room: {
-              name: res.data.name,
-              currentRoom: res.data.title,
-              roomDescription: res.data.description,
-              players: res.data.players
-            }
-          })
-        })
-        .catch(err => err.res)
-    })
-  }
+        this.verifyMovement();
+      } else if (e.key === "d") {
+        this.setState({
+          ...this.state,
+          location: {
+            direction: "e"
+          }
+        });
+        this.verifyMovement();
+      }
+    });
+  };
 
   componentDidUpdate = prevState => {
     if (this.state.gameMapArr !== prevState) {
-      this.drawCanvas()
+      this.drawCanvas();
     }
-  }
+  };
 
   render() {
     return (
@@ -225,11 +231,11 @@ export default class Canvas extends Component {
         <canvas
           ref="canvas"
           id="canvas"
-          width="1040"
-          height="650"
+          width="705"
+          height="705"
           style={{ border: "4px solid green" }}
         ></canvas>
       </div>
-    )
+    );
   }
 }
