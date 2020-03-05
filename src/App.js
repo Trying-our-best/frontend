b5d547debe261771668bfea16844324989c73080
Megan <megan.miller@smoldev.dev>
2020-03-05 14:58:28 -0600

    Updated Maze size

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import React from "react";
+import React from "react"
 import {
   BrowserRouter as Router,
   Route,
   Link,
   Redirect
-} from "react-router-dom";
+} from "react-router-dom"
 
 // component imports
 
-import Login from "./components/Login/Login";
-import Register from "./components/Register/Register";
-import Game from "../src/components/Game/Game";
-import NavBar from "./components/NavBar/NavBar";
+import Login from "./components/Login/Login"
+import Register from "./components/Register/Register"
+import Game from "../src/components/Game/Game"
+import NavBar from "./components/NavBar/NavBar"
 
 // styling & image imports
-import "./App.css";
-import StoneTexture from "./assets/stone-texture.png";
-import Torch from "./assets/torch.png";
+import "./App.css"
+import StoneTexture from "./assets/stone-texture.png"
+import Torch from "./assets/torch.png"
 
 function App() {
   return localStorage.getItem("token") ? (
     <Router>
       <div
         className="App"
-        //   style={{ backgroundImage: `url(${StoneTexture})`, height: "100vh" }}
+        style={{ backgroundImage: `url(${StoneTexture})`, height: "100vh" }}
       >
         <NavBar isLoggedIn={true} />
         <img src={Torch} alt="torch" className="torch-left" />
@@ -37,7 +37,7 @@ function App() {
       <div
         className="App"
         style={{
-          //  backgroundImage: `url(${StoneTexture})`,
+          backgroundImage: `url(${StoneTexture})`,
           height: "100vh"
         }}
       >
@@ -47,7 +47,7 @@ function App() {
         <Route path="/register" component={Register} />
       </div>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Canvas/Canvas.jsx
+++ src/components/Canvas/Canvas.jsx
@@ -1,20 +1,20 @@
-import React, { useState, useEffect, Component } from "react";
+import React, { useState, useEffect, Component } from "react"
 
-import Player from "../../assets/torch.png";
-import PlayerList from "../playerList/playerList";
+import Player from "../../assets/torch.png"
+import PlayerList from "../playerList/playerList"
 
-import { axiosWithAuth } from "../../utils/axiosWithAuth";
+import { axiosWithAuth } from "../../utils/axiosWithAuth"
 
-import Directions from "../Directions";
+import Directions from "../Directions"
 
 export default class Canvas extends Component {
   state = {
-    x: 50,
-    y: 50,
-    torchW: 100,
-    torchH: 100,
-    tileW: 40,
-    tileH: 40,
+    x: 15,
+    y: 15,
+    torchW: 15,
+    torchH: 15,
+    tileW: 15,
+    tileH: 15,
     location: {
       direction: null
     },
@@ -25,36 +25,52 @@ export default class Canvas extends Component {
       players: null
     },
     gameMapArr: []
-  };
+  }
 
   drawCanvas = () => {
-    const ctx = this.refs.canvas.getContext("2d");
-    for (let y = 0; y < 47; y++) {
-      for (let x = 0; x < 47; x++) {
-        if (this.state.gameMapArr[y * 47 + x] > 0) {
-          ctx.fillStyle = "#eee";
+    const ctx = this.refs.canvas.getContext("2d")
+    //draw tiles that make up game map
+    //beign by looping through each tile
+    for (let y = 0; y <= 47; y++) {
+      //for each row, go left to right
+      for (let x = 0; x <= 47; x++) {
+        //switch statement lets us choose which color to draw curent tile with
+        //find value at corresponding game map index by y*mapW and add x
+        // switch (this.state.gameMapArr[y * 47 + x]) {
+        //   case 0:
+        //     ctx.fillStyle = "#999999"
+        //     break
+        //   default:
+        //     ctx.fillStyle = "#eeeeee"
+        // }
+
+        // for length of array find odd rows and remove them.
+
+        if (this.state.gameMapArr[y * 47 + x]) {
+          ctx.fillStyle = "#eee"
         } else {
-          ctx.fillStyle = "#999";
+          ctx.fillStyle = "#999"
         }
+        //draw rectangle at coresponding position tile
         ctx.fillRect(
           x * this.state.tileW,
           y * this.state.tileH,
           this.state.tileW,
           this.state.tileH
-        );
+        )
       }
     }
-  };
+  }
 
   drawTorch = (x, y, width, height) => {
-    const ctx = this.refs.canvas.getContext("2d");
-    // ctx.clearRect(0, 0, this.refs.canvas.width, this.refs.canvas.height);
+    const ctx = this.refs.canvas.getContext("2d")
+    //ctx.clearRect(0, 0, this.refs.canvas.width, this.refs.canvas.height)
 
-    const img = new Image();
-    img.src = Player;
+    const img = new Image()
+    img.src = Player
 
-    ctx.drawImage(img, x, y, width, height);
-  };
+    ctx.drawImage(img, x, y, width, height)
+  }
 
   componentDidMount = () => {
     axiosWithAuth()
@@ -68,9 +84,18 @@ export default class Canvas extends Component {
             name: res.data.name,
             players: res.data.players
           }
-        });
+        })
       })
-      .catch(err => err.err);
+      .catch(err => err.err)
+
+    axiosWithAuth()
+      .get("/api/adv/gamemap/")
+      .then(res => {
+        this.setState({
+          ...this.state,
+          gameMapArr: res.data.gameMap.flat()
+        })
+      })
 
     setInterval(() => {
       this.drawTorch(
@@ -78,17 +103,17 @@ export default class Canvas extends Component {
         this.state.y,
         this.state.torchW,
         this.state.torchH
-      );
-    }, 1000 / 10);
+      )
+    }, 1000 / 30)
 
     document.addEventListener("keydown", e => {
-      let temp;
-      const ctx = this.refs.canvas.getContext("2d");
+      let temp
+      const ctx = this.refs.canvas.getContext("2d")
       // checking for which key is pressed
       if (e.key === "w") {
         // setting temp var to capture value of y
-        temp = this.state.y;
-        temp -= 100;
+        temp = this.state.y
+        temp -= 15
 
         // if y value - 100 is still greater than zero, do movement as normal
         if (temp > 0) {
@@ -99,12 +124,12 @@ export default class Canvas extends Component {
             location: {
               direction: "n"
             }
-          });
+          })
         }
       } else if (e.key === "a") {
         // setting temp var to capture value of y
-        temp = this.state.x;
-        temp -= 100;
+        temp = this.state.x
+        temp -= 15
 
         // if x value - 100 is still greater than zero, do movement as normal
         if (temp > 0) {
@@ -114,25 +139,25 @@ export default class Canvas extends Component {
             location: {
               direction: "w"
             }
-          });
+          })
         }
       } else if (e.key === "s") {
         // setting temp var to capture value of y
-        temp = this.state.y + 100;
+        temp = this.state.y + 15
         // temp += 100
         // if y value - 100 is still greater than zero, do movement as normal
         if (temp < ctx.canvas.height) {
           this.setState({
             ...this.state,
-            y: this.state.y + 100,
+            y: this.state.y + 15,
             location: {
               direction: "s"
             }
-          });
+          })
         }
       } else if (e.key === "d") {
-        temp = this.state.x;
-        temp += 100;
+        temp = this.state.x
+        temp += 15
         // if x value - 100 is still greater than zero, do movement as normal
         if (temp < ctx.canvas.width) {
           this.setState({
@@ -141,7 +166,7 @@ export default class Canvas extends Component {
             location: {
               direction: "e"
             }
-          });
+          })
         }
       }
 
@@ -156,24 +181,15 @@ export default class Canvas extends Component {
               roomDescription: res.data.description,
               players: res.data.players
             }
-          });
-          console.log("moveNorth Res: ", res.data);
+          })
         })
-        .catch(err => {
-          console.log("North Error: ", err.res);
-        });
-    });
+        .catch(err => err.res)
+    })
+  }
 
-    axiosWithAuth()
-      .get("api/adv/gamemap")
-      .then(res => {
-        this.setState({ ...this.state, gameMapArr: res.data.gameMap.flat() });
-      });
-  };
-
-  componentDidUpdate(prevProps, prevState) {
+  componentDidUpdate = prevState => {
     if (this.state.gameMapArr !== prevState) {
-      this.drawCanvas();
+      this.drawCanvas()
     }
   }
 
@@ -210,9 +226,9 @@ export default class Canvas extends Component {
           id="canvas"
           width="1040"
           height="650"
-          style={{ border: "4px solid green", zIndex: 1000 }}
+          style={{ border: "4px solid green" }}
         ></canvas>
       </div>
-    );
+    )
   }
 }
