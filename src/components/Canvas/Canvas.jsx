3ae7b7850e642fb201696218628095e3603f88eb
Megan <megan.miller@smoldev.dev>
2020-03-05 20:52:44 -0600

    updating styles

--- src/components/Canvas/Canvas.jsx
+++ src/components/Canvas/Canvas.jsx
@@ -1,17 +1,16 @@
-import React, { Component } from "react";
+import React, { Component } from "react"
 
 // component imports
-import RoomInfo from '../RoomInfo/RoomInfo'
-import PlayerList from "../playerList/playerList";
+import RoomInfo from "../RoomInfo/RoomInfo"
+import PlayerList from "../playerList/playerList"
+import Win from "../Win/Win"
 
 // utils imports
-import { axiosWithAuth } from "../../utils/axiosWithAuth";
+import { axiosWithAuth } from "../../utils/axiosWithAuth"
 
 // stylesheet & image imports
-import './Canvas.scss'
-import Player from "../../assets/torch.png";
-
-
+import "./Canvas.scss"
+import Player from "../../assets/torch.png"
 
 export default class Canvas extends Component {
   state = {
@@ -33,49 +32,50 @@ export default class Canvas extends Component {
       players: null,
       error_msg: null
     },
-    gameMapArr: []
-  };
+    gameMapArr: [],
+    message: null
+  }
 
   drawCanvas = () => {
-    const ctx = this.refs.canvas.getContext("2d");
+    const ctx = this.refs.canvas.getContext("2d")
     for (let y = 0; y <= 47; y++) {
       for (let x = 0; x <= 47; x++) {
         if (this.state.gameMapArr[y * 47 + x]) {
-          ctx.fillStyle = "#eee";
+          ctx.fillStyle = "#eee"
         } else {
-          ctx.fillStyle = "#999";
+          ctx.fillStyle = "#999"
         }
         ctx.fillRect(
           x * this.state.tileW,
           y * this.state.tileH,
           this.state.tileW,
           this.state.tileH
-        );
+        )
       }
     }
-  };
+  }
 
   drawTorch = (x, y, width, height) => {
-    const ctx = this.refs.canvas.getContext("2d");
-    const img = new Image();
-    img.src = Player;
-    console.log("playerX", this.state.playerX);
-    console.log("playerY", this.state.playerY);
+    const ctx = this.refs.canvas.getContext("2d")
+    const img = new Image()
+    img.src = Player
+    console.log("playerX", this.state.playerX)
+    console.log("playerY", this.state.playerY)
     ctx.drawImage(
       img,
       this.state.playerX * 30 + 15, //0 * 15 * 2 +15= 15      =15
       this.state.playerY * 30 + 15, //1 * 15 * 2 + 15= 45     =30
       width,
       height
-    );
-  };
+    )
+  }
 
   verifyMovement = () => {
     axiosWithAuth()
       .post("api/adv/move/", this.state.location)
       .then(res => {
-        let temp = 0;
-        this.setState({ 
+        let temp = 0
+        this.setState({
           ...this.state,
           playerX: res.data.x,
           playerY: res.data.y,
@@ -86,47 +86,52 @@ export default class Canvas extends Component {
             players: res.data.players,
             error_msg: res.data.error_msg
           }
-        });
+        })
         if (res.data.error_msg === "") {
           switch (this.state.location.direction) {
             case "w":
-              temp = this.state.x;
-              temp -= 30;
+              temp = this.state.x
+              temp -= 30
               this.setState({
                 ...this.state,
                 x: temp
-              });
-              break;
+              })
+              break
             case "e":
-              temp = this.state.x;
-              temp += 30;
+              temp = this.state.x
+              temp += 30
               this.setState({
                 ...this.state,
                 x: temp
-              });
-              break;
+              })
+              break
             case "n":
-              temp = this.state.y;
-              temp -= 30;
+              temp = this.state.y
+              temp -= 30
               this.setState({
                 ...this.state,
                 y: temp
-              });
-              break;
+              })
+              break
             case "s":
-              temp = this.state.y + 30;
+              temp = this.state.y + 30
               this.setState({
                 ...this.state,
                 y: this.state.y + 30
-              });
-              break;
+              })
+              break
             default:
-              break;
+              break
           }
+        } else if (res.data.message) {
+          this.setState({
+            ...this.state,
+            message: res.data.message
+          })
         }
       })
-      .catch(err => err.res);
-  };
+      .catch(err => err.res)
+  }
 
   componentDidMount = () => {
     axiosWithAuth()
@@ -142,9 +147,9 @@ export default class Canvas extends Component {
             name: res.data.name,
             players: res.data.players
           }
-        });
+        })
       })
-      .catch(err => err.err);
+      .catch(err => err.err)
 
     axiosWithAuth()
       .get("/api/adv/gamemap/")
@@ -152,8 +157,8 @@ export default class Canvas extends Component {
         this.setState({
           ...this.state,
           gameMapArr: res.data.gameMap.flat()
-        });
-      });
+        })
+      })
 
     document.addEventListener("keydown", e => {
       // checking for which key is pressed
@@ -163,39 +168,39 @@ export default class Canvas extends Component {
           location: {
             direction: "n"
           }
-        });
-        this.verifyMovement();
+        })
+        this.verifyMovement()
       } else if (e.key === "a") {
         this.setState({
           ...this.state,
           location: {
             direction: "w"
           }
-        });
-        this.verifyMovement();
+        })
+        this.verifyMovement()
       } else if (e.key === "s") {
         this.setState({
           ...this.state,
           location: {
             direction: "s"
           }
-        });
-        this.verifyMovement();
+        })
+        this.verifyMovement()
       } else if (e.key === "d") {
         this.setState({
           ...this.state,
           location: {
             direction: "e"
           }
-        });
-        this.verifyMovement();
+        })
+        this.verifyMovement()
       }
-    });
-  };
+    })
+  }
 
   componentDidUpdate = prevState => {
     if (this.state.gameMapArr !== prevState) {
-      this.drawCanvas();
+      this.drawCanvas()
     }
     if (
       this.state.playerX !== prevState.playerX ||
@@ -206,28 +211,46 @@ export default class Canvas extends Component {
         this.state.y,
         this.state.torchW,
         this.state.torchH
-      );
+      )
     }
-  };
+  }
+
+  reset = () => {
+    axiosWithAuth()
+      .get("/api/adv/reset")
+      .then(res => {
+        this.setState({
+          ...this.state,
+          playerX: res.data.x,
+          playerY: res.data.y,
+          room: {
+            currentRoom: res.data.title,
+            roomDescription: res.data.description,
+            name: res.data.name,
+            players: res.data.players
+          }
+        })
+
+        window.location.reload()
+      })
+  }
 
   render() {
     return (
-      <div className="canvas-wrapper">
-        <canvas ref="canvas" id="canvas" height="705" width="705"></canvas>
+      <div>
+        {this.state.message ? (
+          <Win message={this.state.message} reset={this.reset} />
+        ) : null}
+        <div className="canvas-wrapper">
+          <canvas ref="canvas" id="canvas" height="705" width="705"></canvas>
           {this.state.room ? (
             <div className="Direction-Buttons">
-              {/* <div style={{ backgroundColor: "white" }}> */}
-                {/* {location.direction ? <p>Last Move: {location.direction}</p> : null} */}
-                {/* <p>{this.state.room.currentRoom}</p> */}
-                {/* <p>{this.state.room.roomDescription}</p> */}
-              {/* </div> */}
-              {/* {roomInfo.error_msg ? <p>{roomInfo.error_msg}</p> : null} */}
               <div className="game-info-wrapper">
                 <PlayerList
                   players={this.state.room.players}
                   current={this.state.room.name}
                 />
-                <RoomInfo room={this.state.room}/>
+                <RoomInfo room={this.state.room} />
               </div>
             </div>
           ) : (
@@ -237,11 +260,12 @@ export default class Canvas extends Component {
                   players={this.state.room.players}
                   current={this.state.room.name}
                 />
-                <RoomInfo room={this.state.room}/>
+                <RoomInfo room={this.state.room} />
               </div>
             </div>
           )}
+        </div>
       </div>
-    );
+    )
   }
 }
